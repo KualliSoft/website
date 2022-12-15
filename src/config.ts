ff95175e57b988b75a353d85802875cd6e405dd7
Jonathan Romero <jromeroh93@gmail.com>
2022-12-15 02:41:38 -0600

    Init repository from Astro template

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'KualliSoft - Soluciones digitales';
+export const SITE_DESCRIPTION = 'Apoyamos a MiPyMEs a reducir la brecha digital con soluciones digitales de calidad y accesibles';
